084feb748a528ccdfd4cea7c7edd2f2db769fe65
mafanya001 <79634135+mafanya001@users.noreply.github.com>
2021-03-14 12:45:14 +0300

    fix: add some function

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,6 +1,38 @@
 const CustomError = require("../extensions/custom-error");
 
-module.exports = function transform(/* arr */) {
-  throw new CustomError('Not implemented');
-  // remove line with error and write your code here
+module.exports = function transform(arr) {
+  if (!Array.isArray(arr)) {
+      throw new Error()
+  }
+  const newArr = arr.slice();
+  const reItem = 'item';
+  for (let i = 0; i < arr.length; i++) {
+      switch (arr[i]) {
+          case '--discard-next':
+              if (i < newArr.length - 1) {
+                  newArr[i + 1] = reItem;
+              }
+              newArr[i] = reItem
+              break
+          case '--discard-prev':
+              if (i > 0) {
+                  newArr[i - 1] = reItem
+              }
+              newArr[i] = reItem
+              break;
+          case '--double-next':
+              if (i < newArr.length - 1) {
+                  newArr[i] = newArr[i + 1]
+              } else {
+                  newArr[i] = reItem;
+              }
+              break;
+          case '--double-prev':
+              if (i > 0) {
+                  newArr[i] = newArr[i - 1];
+              } else {
+                  newArr[i] = reItem;
+              }  }  }
+
+  return newArr.filter(el => el !== 'item');
 };
